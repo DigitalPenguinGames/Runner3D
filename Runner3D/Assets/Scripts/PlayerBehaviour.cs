490680115ee7f185b21d12634ff0c1a57dac8d16
kaitokidi <kaaitokid@gmail.com>
2015-11-19 19:53:34 +0100

    rotation and texture on player (?)

---
+++ Runner3D/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float rotationSpeed = 500.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0.0f, 0.0f);	
+	}
+
+}
