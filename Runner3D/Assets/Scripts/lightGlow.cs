f60d60b9c5ad5f55b1098042c33fd9ab390b92d9
kaitokidi <kaaitokid@gmail.com>
2015-11-20 22:21:13 +0100

    get redy to be reckted by a merge

---
+++ Runner3D/Assets/Scripts/lightGlow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lightGlow : MonoBehaviour {
+
+    public float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+
+        float delta = Time.fixedDeltaTime;
+        timer += delta;
+        this.GetComponent<Light>().range = 3 + Mathf.Max(0.3f*Mathf.Sin(timer/2), Mathf.Sin(timer));
+
+
+    }
+}
