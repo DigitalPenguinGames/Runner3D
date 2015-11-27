89c37850ec0904f155402b7b09c33ddff3517b4a
kaitokidi <kaaitokid@gmail.com>
2015-11-27 01:00:50 +0100

    simple and sidest menu

---
+++ Runner3D/Assets/Scripts/menuActor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuActor : MonoBehaviour {
+
+    public bool isStart;
+    public bool isQuit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseUp() {
+        
+        if (isQuit) {
+            Application.Quit();
+        }
+
+        if (isStart) {
+            Application.LoadLevel(1);
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    } 
+}
---
+++ Runner3D/Assets/TextConlorControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextConlorControl : MonoBehaviour {
+
+    void Start()    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    void OnMouseEnter()    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+
+    void OnMouseExit()    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
