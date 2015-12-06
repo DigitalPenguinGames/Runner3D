cfdc45ddcd39581d66a9306df9bef8be07ed5cbb
Pinkii- <pinkiibiza@gmail.com>
2015-12-06 13:37:54 +0100

    updating the UI with the time

---
+++ Runner3D/Assets/Scripts/uiController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class uiController : MonoBehaviour {
+
+	public float time = 0;
+	public float coins = 0;
+	public bool run = false;
+
+	public GameObject ui;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (run) {
+			time += Time.deltaTime;
+			coins += Time.deltaTime;
+		}
+		foreach (uiUpdater up in ui.GetComponentsInChildren<uiUpdater>()) {
+			up.setCoins(Mathf.FloorToInt(coins));
+			up.setTime(time);
+		}
+	}
+}
---
+++ Runner3D/Assets/Scripts/uiUpdater.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class uiUpdater : MonoBehaviour {
+
+	public Text coins;
+	public Text time;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void setCoins(int c) {
+		coins.text = c.ToString();
+	}
+
+	public void setTime(float t) {
+		time.text =  string.Format("{0:#0}:{1:00}.{2:0}",
+		                           Mathf.Floor(t / 60),//minutes
+		                           Mathf.Floor(t) % 60,//seconds
+		                           Mathf.Floor((t*10) % 10));//deciseconds;
+	}
+}
