2f74a505fc2a3e3c75d499a8115583b719e72266
kaitokidi <kaaitokid@gmail.com>
2015-12-11 11:55:38 +0100

    added scene for loading (

---
+++ Runner3D/Assets/LoadingPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadingPlayer : MonoBehaviour {
+
+    public float rotationSpeed = 10; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate( rotationSpeed * Time.deltaTime , 0.0f , 0.0f );
+	}
+
+}
